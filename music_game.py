81915f1ecccbf1962188e86af99b74278e62f6f8
Adam Goodrich <agoodrich57@gmail.com>
2021-07-16 13:24:39 -0400

    added try catch

--- music_game.py
+++ music_game.py
@@ -22,13 +22,17 @@ score = 0
 while another_track:
     try:
         three_rand_songs = random.sample(range(len(list_of_songs)), 3)
-        rando = random.randint(0,2)
+        rando = random.randint(0, 2)
         rando = int(rando)
         track_num = three_rand_songs[rando]
         choice = 1
         pygame.mixer.music.stop()
         pygame.mixer.init()
-        pygame.mixer.music.load(list_of_songs[int(track_num)])
+        try:
+            pygame.mixer.music.load(list_of_songs[int(track_num)])
+        except:
+            pygame.mixer.music.load(list_of_songs[int(track_num)])
+
         pygame.mixer.music.play()
         for i in three_rand_songs:
             rand_song_list = i
@@ -41,7 +45,8 @@ while another_track:
         pygame.mixer.music.play()
         print("\n..............................\n")
         print("some files cause an error where no sound plays. If that happens just hit enter.\n")
-        which_song = input("Which song is playing? 1, 2, or 3? \n\nPress q to quit \n\n")
+        which_song = input(
+            "Which song is playing? 1, 2, or 3? \n\nPress q to quit \n\n")
         rando += 1
         rando = str(rando)
         if which_song == rando:
@@ -52,7 +57,8 @@ while another_track:
             print("\n..............................\n")
         elif which_song == "q":
             print("\n..............................\n")
-            print(f"Thanks for playing the final score was {score} correct answers")
+            print(
+                f"Thanks for playing the final score was {score} correct answers")
             print("\n..............................\n")
             break
         else:
@@ -62,6 +68,4 @@ while another_track:
             print(f"The score is\n\n{score}\n")
             print("\n..............................\n")
     except pygame.error:
-            continue
-            
-
+        continue
